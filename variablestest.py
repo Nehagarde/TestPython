f9ee6ca46d72bc523f0fecafa6de67e0a52ed61c
Nehagarde <neha.r.garde@gmail.com>
2017-12-01 23:54:37 +0530

    Add files via upload

---
+++ variablestest.py
@@ -0,0 +1,30 @@
+# Hello World
+print("Hello World")
+
+# Interactive String Input
+inputstring = input("Enter Some Text: ")
+print(inputstring)
+
+# Basic Arithmetic
+a = input("Enter Value for First Variable: ")
+b = input("Enter Value for Second Variable: ")
+print("First Variable: "+a)
+print("Second Variable: "+b)
+#string concatenation
+sum1 = a + b
+print(sum1)
+#addition
+sum2 = int(a) + int(b)
+print(sum2)
+#subtraction
+diff = int(a) - int(b)
+print(diff)
+#multiplication
+prod = int(a) * int(b)
+print(prod)
+#division
+quo = int(a) / int(b)
+print(quo)
+#modulus
+mod = int(a) % int(b)
+print(mod)

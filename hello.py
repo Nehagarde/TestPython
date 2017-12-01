057723d8c30c3fcff3ad0722ae5adcff12f8d0d4
Nehagarde <neha.r.garde@gmail.com>
2017-12-01 13:21:00 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,24 @@
+
+v1="hello"
+v2=3;v3=2.3
+print ('hello world123')
+print v1
+print v2
+print v3
+
+
+for x in range(5):
+    print x
+for y in range(10,16):
+    print y
+for z in range(3,8,2):
+    print z
+for x in range(1,-15,-2):
+    print x
+
+if (v2==2):
+    print 'hereere --- %d' %v2;
+elif (v2==3):
+    print "invalid"
+else:
+    print "yyyy"        
